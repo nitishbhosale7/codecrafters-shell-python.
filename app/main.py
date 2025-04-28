f79f0c17dc2d96fa452a3d69e7ff3f6d21a0c19a
nitishbhosale7 <nitishbhosale7@gmail.com>
2025-04-28 15:22:08 +0530

    added the repl behaviour to the shell

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 import sys
 
 
+def repl():
+    while True:
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
+
+        # Wait for user input
+        command = input()
+        
+        print(f"{command}: command not found")
+
+
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-
-    # Wait for user input
-    command = input()
-    
-    print(f"{command}: command not found")
+    repl()
     
 
 
