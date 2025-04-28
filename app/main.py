33f093ebcdb7b234bc6bcc1b9d8e6414e07c2f74
nitishbhosale7 <nitishbhosale7@gmail.com>
2025-04-28 15:34:11 +0530

    added the repl behaviour to the shell

--- app/main.py
+++ app/main.py
@@ -1,6 +1,32 @@
 import sys
 
 
+
+class shell():
+    def __init__(self):
+        self.history = []
+        
+        
+    def start(self):
+        self.repl()
+        
+        
+    def repl(self):
+        while True:
+            # Uncomment this block to pass the first stage
+            sys.stdout.write("$ ")
+
+            # Wait for user input
+            command = input()
+            
+            self.history.append(command)
+            
+            print(f"{command}: command not found")
+            
+ 
+            
+            
+
 def repl():
     while True:
         # Uncomment this block to pass the first stage
@@ -13,8 +39,10 @@ def repl():
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    repl()
+    
+    shell = shell()
+    
+    shell.start()
     
 
 
