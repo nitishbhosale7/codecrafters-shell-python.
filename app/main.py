0cc44f1a841e56da6e2f35db04a61f5adda406d7
nitishbhosale7 <nitishbhosale7@gmail.com>
2025-04-29 20:49:45 +0530

    adding execution with the quote

--- app/main.py
+++ app/main.py
@@ -92,8 +92,8 @@ class shell:
                 
                 if command_path:
                     # Use subprocess to handle more complex command execution
-                    os.system(command_path, [command] + args)
-                    pass
+                    os.execvp(command_path, [command] + args)
+                    print('nitish');
                     
                     
                 else:
